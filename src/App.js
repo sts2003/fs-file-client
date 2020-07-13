4dde985018bcb811d7bcf6c1ae9f328ea1848210
sts2003 <44109148+sts2003@users.noreply.github.com>
2020-07-13 22:30:24 +0900

    20200713

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from "react-dom";
+import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
+  componentDidMount = async () => {
+    const data1 = "하이";
+    const data2 = "바이";
+
+    const inputData = {
+      data1: data1,
+      data2: data2,
+    };
+
+    await axios.post(
+      "/api/test",
+      {
+        params: { inputData },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  };
   render() {
     return (
       <div>
